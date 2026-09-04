3d3ac082598f0fb363465b2262659f58c3e998c1
Eugenio Tampieri <eugenio@eutampieri.eu>
2026-09-04 17:42:51 +0200

    Unibo: Get Areas

--- beecal-common/index.ts
+++ beecal-common/index.ts
@@ -1,3 +1,12 @@
 export interface TimetableProvider {
     institutionName: string,
+    contry: string,
+    license: string,
+
+    getAreas(): Promise<Area[]>,
+}
+
+export interface Area {
+    name: string,
+    id: string,
 }
---
+++ providers/beecal-unibo/index.ts
@@ -0,0 +1,50 @@
+import { Area, TimetableProvider } from "beecal-common/dist";
+import temporaryDirectory from "temp-dir";
+import fs from "node:fs/promises";
+import fs_stream from "node:fs";
+import csv from "csv-parser";
+
+const OPENDATA_DIR = `${temporaryDirectory}/beecal-unibo`;
+const OPENDATA_FILE = `${OPENDATA_DIR}/corsi.csv`;
+const OPENDATA_VERSION = `${OPENDATA_DIR}/info`;
+
+export class UniboProvider implements TimetableProvider {
+    institutionName: string = "Alma Mater Studiorum - Università di Bologna";
+    contry: string = "it";
+    license: string = "CC-BY-3.0-IT";
+
+    async #fetchOpenData() {
+        // Ensure open data dir presence
+        await fs.mkdir(OPENDATA_DIR, { recursive: true });
+        let response = await fetch("https://dati.unibo.it/api/3/action/package_show?id=degree-programmes").then(x => x.json());
+        let datasets = response.result.resources;
+        let current = datasets[0];
+        let currentVersion = `${current.id}|${current.revision_id}|${current.last_modified}`;
+        let savedVersion = await fs.readFile(OPENDATA_VERSION, { encoding: "utf-8" }).catch(() => "")
+        if (savedVersion != currentVersion) {
+            let data = await fetch(current.url).then(x => x.text());
+            await fs.writeFile(OPENDATA_FILE, data);
+            await fs.writeFile(OPENDATA_VERSION, currentVersion);
+        }
+    }
+
+    async getAreas(): Promise<Area[]> {
+        const results: string[] = [];
+        return new Promise<Area[]>((res, _) => {
+            fs_stream.createReadStream(OPENDATA_FILE)
+                .pipe(csv())
+                .on("data", (data) => {
+                    if (data.ambiti != "") {
+                        results.push(data.ambiti)
+                    }
+                })
+                .on("end", () => {
+                    res(
+                        Array.from(new Set(results)).sort()
+                            .map((x: string) => { return { id: x, name: x } })
+                    );
+                });
+        });
+
+    }
+}
