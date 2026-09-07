dfe9ddec13fdfd17bf1d5a121d0da33fdb128229
Eugenio Tampieri <eugenio@eutampieri.eu>
2026-09-07 11:18:10 +0200

    Devel proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3002/api"
+      }
+    }
+  },
   plugins: [
     vue(),
     vueDevTools(),
